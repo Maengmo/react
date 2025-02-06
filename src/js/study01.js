2734f9c6fbbc3ac94ccf604e7bf140771a5279a9
kim.daehwan1 <kim.daehwan1@joins.com>
2025-02-06 17:51:42 +0900

    feat: React 컴포넌트 & 생명주기 함수

---
+++ src/js/study01.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom/client';
+
+class StudyReact extends React.Component {
+    render() {
+        return (
+            <h5>Study React {this.props.type} {this.props.user} </h5>
+        )
+    }
+}
+
+
+
+
+
